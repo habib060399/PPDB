27d69c625cae108b2346b5d978bd9e8a23a5cf4f
habib060399 <habibsh180@gmail.com>
2023-10-13 17:15:47 +0700

    menambahkan model siswa

---
+++ app/Models/Siswa.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'form_siswa';
+
+    protected $fillable = [
+        'nama_lengkap',
+        'nisn',
+        'ttl',
+        'alamat',
+        'jenkel',
+        'anakke',
+        'agama',
+        'asal_sekolah',
+        'negara',
+        'no_reg',
+        'nama_ortu',
+        'no_kk',
+        'no_hp_ortu',
+        'alamat_ortu'
+    ];
+
+    public $timestamps = false;
+
+}
