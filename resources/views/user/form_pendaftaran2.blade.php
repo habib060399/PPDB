cab7f25167bd1f737534c6a17dd15c45d83fb245
habib060399 <habibsh180@gmail.com>
2023-10-16 19:54:33 +0700

    menambahkan atribut name pada setiap input

--- resources/views/user/form_pendaftaran2.blade.php
+++ resources/views/user/form_pendaftaran2.blade.php
@@ -7,53 +7,54 @@
                             <div class="card-body">
                                 <h4 class="m-t-0 m-b-30">Form Pendaftaran</h4>
 
-                                <form class="form-horizontal">
+                                <form class="form-horizontal" action="">
+                                    @csrf
                                     <div class="form-group row">
                                         <label class="col-sm-2 control-label" for="example-text-input">Asal Sekolah</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="example-text-input">
+                                            <input type="text" class="form-control" id="example-text-input" name="asal_sekolah">
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         <label class="col-sm-2 control-label" for="example-text-input">Kewarganegaraan</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="example-text-input">
+                                            <input type="text" class="form-control" id="example-text-input" name="warganegara">
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         <label class="col-sm-2 control-label" for="example-text-input">No Registrasi akta kelahiran</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="example-text-input">
+                                            <input type="text" class="form-control" id="example-text-input" name="no_reg_akta">
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         <label class="col-sm-2 control-label" for="example-text-input">Nama orangtua Ayah/Ibu</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="example-text-input">
+                                            <input type="text" class="form-control" id="example-text-input" name="nama_ortu">
                                         </div>
                                     </div>
                                           
                                     <div class="form-group row">
                                         <label class="col-sm-2 control-label" for="example-text-input">No Kartu Keluarga</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="example-text-input">
+                                            <input type="text" class="form-control" id="example-text-input" name="no_kk">
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         <label class="col-sm-2 control-label" for="example-text-input">No Hp orang tua</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="example-text-input">
+                                            <input type="text" class="form-control" id="example-text-input" name="no_hp">
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         <label class="col-sm-2 control-label" for="example-text-input">Alamat Orang tua</label>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control" id="example-text-input">
+                                            <input type="text" class="form-control" id="example-text-input" name="alamat_ortu">
                                         </div>
                                     </div>   
                                 </br>
