aa78623e04bcf8a8d41d0a20918b03758f808b55
habib060399 <habibsh180@gmail.com>
2023-10-22 13:43:45 +0700

    mengubah const home

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/user';
     // public const HOME = '/home';
 
     /**
