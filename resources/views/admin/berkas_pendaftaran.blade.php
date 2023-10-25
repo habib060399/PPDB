06791652c0009932410cdcf79a9eec3d23b35b3c
Abib <habibsh180@gmail.com>
2023-10-25 17:00:02 +0700

    menampilkan data berkas

--- resources/views/admin/berkas_pendaftaran.blade.php
+++ resources/views/admin/berkas_pendaftaran.blade.php
@@ -11,63 +11,63 @@
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Nama Lengkap</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->nama_lengkap}}" readonly>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Nisn</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->nisn}}" readonly>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Tanggal Lahir</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->ttl}}" readonly>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Alamat</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->alamat}}" readonly>
                         </div>
                     </div>
                           
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Jenis Kelamin</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->jenkel}}" readonly>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Anak ke-</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->anakke}}" readonly>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Agama</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->agama}}" readonly>
                         </div>
                     </div>     
 
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Asal Sekolah</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->asal_sekolah}}" readonly>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Kewarganegaraan</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->negara}}" readonly>
                         </div>
                     </div>
 
@@ -81,28 +81,28 @@
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Nama orangtua Ayah/Ibu</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->nama_ortu}}" readonly>
                         </div>
                     </div>
                           
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">No Kartu Keluarga</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->no_kk}}" readonly>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">No Hp orang tua</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->no_hp_ortu}}" readonly>
                         </div>
                     </div>
 
                     <div class="form-group row">
                         <label class="col-sm-2 control-label" for="example-text-input">Alamat Orang tua</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="example-text-input" readonly>
+                            <input type="text" class="form-control" id="example-text-input" value="{{$berkas->alamat_ortu}}" readonly>
                         </div>
                     </div>   
                 </br>
